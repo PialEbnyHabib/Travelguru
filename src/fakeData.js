a4b6fb653deabee24d1dde1bd8f4af09be9d5dc9
pial ebny habib <khanmd.pialebnyhabib@gmail.com>
2020-09-27 00:08:40 +0600

    final

---
+++ src/fakeData.js
@@ -0,0 +1,21 @@
+var fakeData = [
+    {
+        name: 'Saint Martin',
+        description: 'Visit the most beutiful place in bangladesh.It is an iceland surrounding with beuty.explore and enjooy!',
+        picture: 'https://i.pinimg.com/originals/1a/80/df/1a80dfafa8ad6333ad0eb63524cad5ec.jpg',
+        id: 1
+    },
+    {
+        name: 'Sylhet',
+        description: 'Place with a lot of visint spoot.There have plenty of placec to visit here in sylhet.one of the most exploring place of our country.',
+        picture: 'https://media-eng.dhakatribune.com/uploads/2019/02/sf-feb24-19-pg8-1-1551015313634.jpg',
+        id: 2
+    },
+    {
+        name: 'Bandarban',
+        description: 'Bandarban is situated in the south of our country.Entire district iss filled with beaty of nature.nafakum,niliri,boga lake and other wonderfull visiting spot are here in bandarban.',
+        picture: 'https://www.daily-sun.com/assets/news_images/2016/06/27/Daily-Sun-Magazine-18.jpg'
+    },
+]
+
+export default fakeData;
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCENHeQoy7F_tL8ido4RdtVf3zqRuuc-TM",
+    authDomain: "travelguru-d5855.firebaseapp.com",
+    databaseURL: "https://travelguru-d5855.firebaseio.com",
+    projectId: "travelguru-d5855",
+    storageBucket: "travelguru-d5855.appspot.com",
+    messagingSenderId: "495030050644",
+    appId: "1:495030050644:web:9e916acc7d803fadb372c1"
+  };
+
+  export default firebaseConfig; 
